05a281d1a7c307a9a3ce97336abf48f7d6f28227
EnCloud <dnsrl1753@gmail.com>
2017-07-22 19:25:46 +0900

    second

--- Assets/2.Scripts/S10_StartMenu.cs
+++ Assets/2.Scripts/S10_StartMenu.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
-public class S10_StartMenu : NetworkBehaviour
+public class S10_StartMenu : MonoBehaviour
 {
-    // FIXME :
     public S00_LobbyManager s00_LobbyManager;
 
     public void OnClickStart()
     {
-        // FIXME : 
         s00_LobbyManager.StartMatchMaker();
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene("LobbyScene");
     }
 }
